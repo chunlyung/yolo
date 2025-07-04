80ac85a546f385e626e42924fb6f585cae188a4d
jaehun <leerungab@naver.com>
2025-07-04 20:06:51 +0900

    수정

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+console.log('DB USER',process.env.DB_USER);
+
 const express = require('express');
 const session = require('express-session');
 const mysql = require('mysql2/promise');
@@ -15,11 +19,11 @@ app.use(session({
   resave: false,
   saveUninitialized: false , // 빈 세션 저장 안함 
   store: new MySQLStore({
-    host:'localhost',
-    port:3306,
-    user:'root',
-    password:'111111',
-    database:'yolos'
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
   }),
   cookie: {
     httpOnly:true,
--- db.js
+++ db.js
@@ -1,11 +1,20 @@
+require('dotenv').config(); 
+
 const mysql = require('mysql2/promise');
+ console.log('CONNECTING TO:',{
+ host:process.env.DB_HOST,       // 또는 DB 서버 IP
+    user:process.env.DB_USER,            // 너의 MySQL 계정
+    password:process.env.DB_PASSWORD, // 비밀번호
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT  });
 
 async function getConnection() {
   const connection = await mysql.createConnection({
-    host: 'localhost',       // 또는 DB 서버 IP
-    user: 'root',            // 너의 MySQL 계정
-    password: '111111', // 비밀번호
-    database: 'yolos',  
+    host:process.env.DB_HOST,       // 또는 DB 서버 IP
+    user:process.env.DB_USER,            // 너의 MySQL 계정
+    password:process.env.DB_PASSWORD, // 비밀번호
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT  
   })
   return connection;
 }
