41ea1010a2244b66a1933df97c5f88dea8bc5257
jaehun <leerungab@naver.com>
2025-07-08 02:16:54 +0900

    수정

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(session({
     port:process.env.DB_PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    timezone:'Asia/seoul'
   }),
   cookie: {
     httpOnly:true,
