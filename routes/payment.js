d6a7952151cb10b8c3c3c54c0e0f6f9a98af7e66
jaehun <leerungab@naver.com>
2025-07-09 22:54:07 +0900

    수정

--- routes/payment.js
+++ routes/payment.js
@@ -6,7 +6,7 @@ const getConnection = require("../db");
 router.get("/success", async (req, res) => {
   const { paymentKey, orderId, amount } = req.query;
   const db = await getConnection();
-  const secretKey = "test_gsk_vZnjEJeQVxayk77QB1yzrPmOoBN0";
+  const secretKey = "live_sk_ALnQvDd2VJogavjbZJyYrMj7X41m";
 
   try {
     // ✅ 토스 결제 인증
