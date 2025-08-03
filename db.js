b8bf3dc03d0b39561c505c1167b49371717710c2
jaehun <leerungab@naver.com>
2025-08-03 16:54:37 +0900

    수정

--- db.js
+++ db.js
@@ -1,41 +1,23 @@
-require('dotenv').config();
-const mysql = require('mysql2/promise');
+require('dotenv').config(); 
 
-console.log('CONNECTING TO:', {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const mysql = require('mysql2/promise');
+ console.log('CONNECTING TO:',{
+ host:process.env.DB_HOST,       // 또는 DB 서버 IP
+    user:process.env.DB_USER,            // 너의 MySQL 계정
+    password:process.env.DB_PASSWORD, // 비밀번호
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT  });
 
 async function getConnection() {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-      timezone: '+09:00'
-    });
-
-    // 🔴 여기 추가: 연결 끊김 자동 감지 및 재연결 시도
-    connection.on('error', async (err) => {
-      console.error('MySQL 연결 오류 발생:', err);
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        console.log('🔁 DB 연결 끊김 -> 재연결 시도');
-        await getConnection(); // 재연결 시도
-      } else {
-        throw err;
-      }
-    });
-
-    return connection;
-  } catch (err) {
-    console.error('❌ DB 연결 실패:', err);
-    throw err;
-  }
+  const connection = await mysql.createConnection({
+    host:process.env.DB_HOST,       // 또는 DB 서버 IP
+    user:process.env.DB_USER,            // 너의 MySQL 계정
+    password:process.env.DB_PASSWORD, // 비밀번호
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT,
+    timezone:'+09:00'
+  })
+  return connection;
 }
 
 module.exports = getConnection;
